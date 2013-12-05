88f296879dfd39aa2ed929c40555a645c4d5d170
Quinn Romanek <quinnromanek@gmail.com>
2013-12-04 18:03:39 -0800

    Added compare command

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 roomdraw
 ========
 
-Room Draw Simulator
+Room Draw Simulator: 
+
+## Commands
+
+*End*
+'end' : Closes the progam
+
+*Points*
+'points [lastname]' : Prints the points of the brother
+
+*Room*
+'room [roomname]' : Prints the current occupants of the room and their respective points.
+
+*Check*
+
+'check [roomname] [list of brother lastnames]' : Prints whether or not the given list of brothers can take the room. *Squatter points are not accounted for*
+
+*Take*
+'take [roomname] [list of brother lastnames]' : Takes the room with given brothers if possible, otherwise tells you that the room can't be taken.
+
+*Compare*
+'compare [list of brother lastnames]' : Prints all the possible combinations of brothers that could take a room from the given list of brothers.   It doesn't take into account brothers that have already taken rooms, however.
--- roomdraw.py
+++ roomdraw.py
@@ -1,26 +1,38 @@
 import csv
+
+class Person:
+	def __init__(self, name, points, room=None):
+		self.name = name
+		self.points = points
+		self.room = room
+
 class Room: 
-	def __init__(self, members):
+	def __init__(self, members, name):
 		self.size = members
+		self.name = name
 		self.occupants = []
 
 	def occupy(self, new_occupants):
 		if len(new_occupants) is not self.size:
 			return False
+		for o in self.occupants:
+			o.room = None
+		for o in new_occupants:
+			o.room = self.name
 		self.occupants = new_occupants
 		return True
 
 	def total_points(self):
 		points = 0
 		for brother in self.occupants:
-			points += brother[1]
+			points += brother.points
 
 		return points
 
 	def can_take(self, occupants):
 		taking_points = 0
 		for brother in occupants:
-			taking_points += brother[1]
+			taking_points += brother.points
 
 		return (taking_points > self.total_points()) and len(occupants) == self.size
 
@@ -35,9 +47,41 @@ class Room:
 		if len(self.occupants) == 0:
 			return "No one is in this room."
 		for brother in self.occupants:
-			val += "{0}: {1} pts".format(brother[0].capitalize(), brother[1])
+			val += "{0}: {1} pts".format(brother.name.capitalize(), brother.points)
 
 		return val
+
+def compare(occupants):
+	target = len(occupants)
+	names = map(lambda x: x.name, occupants)
+	points = 0
+	for o in occupants:
+		points += o.points
+	combos = []
+	def comparison(total, depth, already, checked):
+
+		if total > points:
+			others = target - depth + 1 
+			word = ""
+			for a in already:
+				word += (a.capitalize() + " ") 
+			if(others > 0): word += "and {0} other(s)".format(others)
+
+			print(word)
+			return
+		elif depth > target:
+			return
+		local_checked = []
+		for b in BROTHERS:
+			bro = BROTHERS[b]
+			if bro.room != None or bro.name in names or bro.name in already or bro.name in checked: continue
+			comparison(total + bro.points, depth + 1, already + [bro.name], checked + local_checked)
+			local_checked.append(bro.name)
+
+	comparison(0, 1, [], [])
+
+
+	
 
 ROOMS = {}
 BROTHERS = {}
@@ -46,23 +90,27 @@ triples = ['2b', '2d', '3b', '3d']
 quads = ['2c', '3c']
 singles = ['c2', '2g', '3g']
 for d in doubles:
-	ROOMS[d] = Room(2)
+	ROOMS[d] = Room(2, d)
 
 for t in triples:
-	ROOMS[t] = Room(3)
+	ROOMS[t] = Room(3, t)
 
 for q in quads:
-	ROOMS[q] = Room(4)
+	ROOMS[q] = Room(4, q)
 
 for s in singles:
-	ROOMS[s] = Room(1)
+	ROOMS[s] = Room(1, s)
 
 with open('points.csv', 'rb') as file:
 	reader = csv.reader(file)
 	for row in reader:
-		BROTHERS[row[1].lower()] = int(row[2])
+		BROTHERS[row[1].lower()] = Person(row[1].lower(), int(row[2]))
+		if row[1].lower() in ['koury', 'bernhart']: BROTHERS[row[1].lower()].room = "Out"
 global run
 run = True
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("~~~     Room Draw Simulator       ~~~")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 while run:
 
 	text = raw_input("~>")
@@ -78,7 +126,7 @@ while run:
 			print "Error: Unrecognized Room Name"
 	elif name == "points":
 		try:
-			print("{0}: {1} points".format(args[1], BROTHERS[args[1].lower()]))
+			print("{0}: {1} points".format(args[1], BROTHERS[args[1].lower()].points))
 		except KeyError:
 			print "Error: Unknown Brother.  Make sure to only use last names."
 	elif name == "take":
@@ -86,11 +134,11 @@ while run:
 		occupants = []
 		room = ROOMS[args[1]]
 		for b in brothers:
-			occupants.append([b.lower(), BROTHERS[b.lower()]])
+			occupants.append(BROTHERS[b.lower()])
 
 		result = room.try_take(occupants)
 		if result == True:
-			word = "{0} taken by ".format(args[1])
+			word = "{0} taken by ".format(args[1].capitalize())
 			for b in brothers:
 				word += (b + " ")
 			print word
@@ -101,12 +149,18 @@ while run:
 		occupants = []
 		room = ROOMS[args[1]]
 		for b in brothers:
-			occupants.append([b.lower(), BROTHERS[b.lower()]])
+			occupants.append(BROTHERS[b.lower()])
 		result = room.can_take(occupants)
 		if result:
 			print("It's possible to take {0} with these brothers.".format(args[1]))
 		else:
 			print("Not enough points.")
+	elif name == "compare":
+		brothers = args[1:]
+		occupants = []
+		for b in brothers:
+			occupants.append(BROTHERS[b.lower()])
+		compare(occupants)
 
 
 
